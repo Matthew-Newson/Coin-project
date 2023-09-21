eba41f77e884fdd2dd8288d1d08b09a3b257e38e
Ffaaces <144438610+Ffaaces@users.noreply.github.com>
2023-09-21 17:24:31 +0100

    Add files via upload

---
+++ Report/Coin Counter.py
@@ -0,0 +1,10 @@
+Name = input("What is your name? ")
+Coin = int(input("What is the value of the coin in the bag? "))
+Weight = float(input("What is the weight of the bag? "))
+
+Dictionary = {
+    "1p" : 356.00
+    "2p" : 
+    "5p" : 325.00
+}
+print()
